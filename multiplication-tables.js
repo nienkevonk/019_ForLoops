4b632d28ef8117cb80799b1ddbab58ca30ffc5b7
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-20 14:51:05 +0200

    Add files via upload

---
+++ multiplication-tables.js
@@ -0,0 +1,12 @@
+var multiplier = 9;
+for (var i = 0; i <= 10; i++) {
+    var result = multiplier * i;
+    console.log(multiplier + '*' + i + '=' + result);
+}
+
+for (var multiplier = 0; multiplier <= 10; multiplier++) {
+    for (var i = 0; i <= 10; i++) {
+        var result = multiplier * i;
+        console.log(multiplier + '*' + i + '=' + result)
+    }
+}
